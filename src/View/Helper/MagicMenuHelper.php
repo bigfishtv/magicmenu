67bfc76bed3ae786403b9b196d1c5dac13e2167f
Adam Royle <adam@bigfish.tv>
2015-06-24 14:08:45 +1000

    Initial commit.

---
+++ src/View/Helper/MagicMenuHelper.php
@@ -0,0 +1,13 @@
+<?php
+namespace MagicMenu\View\Helper;
+
+use Cake\View\Helper;
+
+class MagicMenuHelper extends Helper
+{
+
+    public function hello() {
+        return 'Hello, world!';
+    }
+    
+}
